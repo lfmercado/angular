dac70ca6107f7c4498e370ef4996298a23f8f231
Mercado <luis.mercado@accenture.com>
2018-07-27 10:55:33 -0500

    nuevoCommit

--- src/app/externo/externo.component.ts
+++ src/app/externo/externo.component.ts
@@ -11,36 +11,54 @@ export class ExternoComponent implements OnInit {
 
   public user: any;
   public userId: any;
-  public noValid:boolean;
-  public fecha:any;
+  public noValid: boolean;
+  public fecha: any;
+  public new_user: any;
+  public usuario_guardado:any;
 
 
-  constructor(private _peticionesServices:PeticionesServices) { }
+  constructor(private _peticionesServices: PeticionesServices) { }
 
   ngOnInit() {
-    this.userId =1;
-    this.noValid= false;
+    this.userId = 1;
+    this.noValid = false;
+    this.new_user = {
+      "name": "",
+      "job": ""
+    };
     this.fecha = new Date(2019, 5, 20);
   }
 
-  cargarUsuario(){
+  cargarUsuario() {
     console.log(this.userId);
-    if(this.userId != ''){
+    if (this.userId != '') {
       this.noValid = false;
-    this._peticionesServices.getUser(this.userId).subscribe(
-      resultado =>{
-        this.user = resultado.data;
-        console.log(resultado);
-      }, 
-      error => {
-        console.log(<any>error);
-      }
-      
-    );
-    }else {
-    this.noValid = true;
+      this._peticionesServices.getUser(this.userId).subscribe(
+        resultado => {
+          this.user = resultado.data;
+          console.log(resultado);
+        },
+        error => {
+          console.log(<any>error);
+        }
+
+      );
+    } else {
+      this.noValid = true;
     }
-}
- 
+  }
+  onSubmit(form){
+    this._peticionesServices.addUser(this.new_user).subscribe(
+      response  =>{
+        console.log(response);
+        this.usuario_guardado = response;
+        form.reset();
+      },
+      error =>{
+        console.log(error);
+      }
+    );
+  }
+
 
 }
--- src/app/service/peticiones.service.ts
+++ src/app/service/peticiones.service.ts
@@ -15,4 +15,9 @@ export class PeticionesServices{
     getUser(userId):Observable<any>{
         return this._http.get(this.url + 'api/users/' + userId);
     }
+    addUser(user):Observable<any>{
+        let params = JSON.stringify(user);
+        let headers =   new HttpHeaders().set('Content-Type', 'application/json');
+        return this._http.post(this.url+ 'api/users', params, {headers: headers});
+    }
 }
